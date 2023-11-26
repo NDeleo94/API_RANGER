389b6a3574fae0930887d2c8e7cc6a82fadd0b85
NDeleo94 <n.deleo94@gmail.com>
2023-11-26 01:53:40 -0300

    Se agrega parametros de busqueda

--- helpers/actor.js
+++ helpers/actor.js
@@ -8,6 +8,16 @@ const actorFilterParams = (params) => {
             = { [Op.substring]: params.fullname };
     }
 
+    if (params.birthday_s && params.birthday_e) {
+        filter.birthday
+            = { [Op.between]: [params.birthday_s, params.birthday_e] };
+    }
+
+    if (params.deathday_s && params.deathday_e) {
+        filter.deathday
+            = { [Op.between]: [params.deathday_s, params.deathday_e] };
+    }
+
     return filter
 }
 
